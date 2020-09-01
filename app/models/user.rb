4bfcf5ad6684807cb51364250eba6b3f9d050a7e
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-09-01 15:32:26 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+      validates :family_name_kanji
+      validates :first_name_kanji
+    end
+
+    with_options format: { with: /\A[ァ-ン]+\z/, message: '全角文字(カタカナ)を使用してください' }
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+
+  end
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
