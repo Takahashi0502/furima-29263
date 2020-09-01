72c774a8eaf5ff689178b1217c37666cead07fc6
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-09-01 20:05:08 +0900

    rubocopによる表記の修正指示を反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
-    
+
     with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
       validates :family_name_kanji
       validates :first_name_kanji
@@ -17,9 +17,8 @@ class User < ApplicationRecord
       validates :family_name_kana
       validates :first_name_kana
     end
-
   end
-  
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
