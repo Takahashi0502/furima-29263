b209cff3e1174cc61a5b9ad375e4617ebceb9849
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-09-03 15:04:03 +0900

    出品機能のコントローラー部分の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: :index
   def index
   end
 
@@ -8,6 +9,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.create(item_params)
+    binding.pry
     if @item.save
       redirect_to root_path
     else
@@ -18,6 +20,11 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :info, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :shipping_days_id, :price)
+    params.require(:item).permit(:name, :info, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :shipping_days_id, :price).merge(user: current_user )
+  end
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
