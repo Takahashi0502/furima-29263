46bf09a9be1fd592ceecabc4ba079c19061f881b
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-09-03 15:17:23 +0900

    出品機能のコントローラー部分の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: :index
   def index
   end
 
@@ -18,6 +19,11 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :info, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :shipping_days_id, :price)
+    params.require(:item).permit(:name, :info, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :shipping_days_id, :price).merge(user: current_user)
+  end
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
