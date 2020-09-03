4d304c4d8e65d1c8bece845731643669a2e692aa
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-09-03 11:37:39 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :shipping_days_id, :price)
+  end
 end
