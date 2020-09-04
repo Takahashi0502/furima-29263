b6e00f124964883397b47d6b2ed8a45a04b1de63
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-09-04 18:01:32 +0900

    テストコードの追記

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,11 +34,23 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
-    
+
+    it 'category_idの値が1だと登録できない' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Category リストから選択してください')
+    end
+
     it 'status_idが空だと登録できない' do
       @item.status_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Status can't be blank")
+    end
+
+    it 'status_idの値が1だと登録できない' do
+      @item.status_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Status リストから選択してください')
     end
 
     it 'delivery_fee_idが空だと登録できない' do
@@ -47,10 +59,22 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
     end
 
+    it 'delivery_fee_idの値が1だと登録できない' do
+      @item.delivery_fee_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Delivery fee リストから選択してください')
+    end
+
     it 'shipping_area_idが空だと登録できない' do
       @item.shipping_area_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping area can't be blank")
+    end
+
+    it 'shipping_area_idの値が1だと登録できない' do
+      @item.shipping_area_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Shipping area リストから選択してください')
     end
 
     it 'shipping_days_idが空だと登録できない' do
@@ -59,20 +83,32 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Shipping days can't be blank")
     end
 
+    it 'shipping_days_idの値が1だと登録できない' do
+      @item.shipping_days_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Shipping days リストから選択してください')
+    end
+
     it 'priceが空だと登録できない' do
       @item.price = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Price can't be blank")
     end
 
-    it 'priceが既定の金額外だと登録できない' do
+    it 'priceが300円未満だと登録できない' do
       @item.price = 200
       @item.valid?
       expect(@item.errors.full_messages).to include('Price 指定内の金額を入力してください')
     end
 
+    it 'priceが10000000円以上だと登録できない' do
+      @item.price = 10_000_000
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price 指定内の金額を入力してください')
+    end
+
     it 'priceが半角数字以外だと登録できない' do
-      @item.price = "aaa"
+      @item.price = 'aaa'
       @item.valid?
       expect(@item.errors.full_messages).to include('Price 指定内の金額を入力してください')
     end
