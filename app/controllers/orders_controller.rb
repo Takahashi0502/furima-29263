b41fbcceee72181cad62c0280087a213763bcd7e
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-09-10 14:27:38 +0900

    Payjpでの決済の流れをコントローラーに記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,14 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(user_id: current_user.id, item_id: @item.id)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
@@ -12,4 +20,17 @@ class OrdersController < ApplicationController
   def set_item
     @item = Item.find(params[:item_id])
   end
+
+  def order_params
+    params.permit(:token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency:'jpy'
+   )
+ end
 end
