de98116742fff22b8c05cbd99f52f8823f196c92
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-09-14 18:06:08 +0900

    リポジトリの名称を修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'furima-29263'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:progate-begin/furima-29263.git'
+set :repo_url,  'git@github.com:progate-bigin/furima-29263.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
